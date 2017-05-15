73af48df630150f0eaee1159d2e652144842fad9
mlauf <stud.michael.lauf@fh-kufstein.ac.at>
2017-05-15 12:32:53 +0200

    router_test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import { Button } from 'react-bootstrap';
+import { Router, Route, Switch } from 'react-router'
 
 class App extends Component {
   render() {
