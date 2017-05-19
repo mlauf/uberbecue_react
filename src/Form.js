c67403a31778cce844ced88e1159f358cc7558dd
Manuel Höflinger <stud.manuel.hoeflinger@fh-kufstein.ac.at>
2017-05-19 10:05:24 +0200

    Form.js handler erstellt

--- src/Form.js
+++ src/Form.js
@@ -4,18 +4,41 @@
 import React, { Component } from 'react';
 
 class Form extends Component{
+    constructor(props){
+        super(props);
+        this.state = {grillname: ''};
+        this.state = {grilldescription:''};
+        this.state = {grilllat: ''};
+        this.state = {grilllong: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSumit = this.handleSumit.bind(this);
+    }
+    handleChange(event){
+        this.setState({grillname: event.target.grillname});
+        this.setState({grilldescription: event.target.grilldescription});
+        this.setState({grilllat: event.target.grilllat});
+        this.setState({grilllong: event.target.grilllong});
+    }
+    handleSumit(event){
+        alert(this.state.gillname +' angelegt!')
+    }
     render(){
-        <form>
+        <form class="from-group" onSubmit={this.handleSumit}>
             <label>
                 Grill:
-                <input type="text" name="grillname"/>
+                <input class="form-control" type="text" name="grillname" onChange={this.handleChange}/>
+            </label>
+            <label>Beschreibung:
+                <input class="form-control" type="textarea" name="grilldescription" onChange={this.handleChange}/>
             </label>
             <label>
                 Position:
-                <input type="text" name"grilllat" value="lat"/><inputtype="text" name"grilllong" value="long"/>
+                <input class="form-control" type="text" name"grilllat" value="lat" onChange={this.handleChange} />
+                <input type="text" name"grilllong" value="long" onChange={this.handleChange}/>
             </label>
 
-            <input type="submit" value="Anlegen" action="localhost:3000/api/grill"/>
+            <input type="submit" value="Anlegen" class="btn btn-success" />
         </form>
     }
 }
